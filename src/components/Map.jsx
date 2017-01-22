7016385680af74f76b9bd4cd3db9f40b7c497458
JH Kuipers <jd@leetbook>
2017-01-22 22:07:56 +0100

    test points

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,7 +2,7 @@
  * Created by jd on 22-1-17.
  */
 import React, {Component} from 'react'
-import ReactMapboxGl, { Layer, Feature, ZoomControl, ScaleControl } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, ZoomControl, ScaleControl, Marker } from "react-mapbox-gl";
 import OSMStyle from '../mapstyles/osmbright_style.js'
 
 let style = OSMStyle;
@@ -14,8 +14,11 @@ style.sources = {
     }
 };
 
-let datapoint = {'lane': 'lane2', 'measurementSiteId': 'GEO02_R_RWSTI610', 'measurementTime': '2017-01-20T06:11:00Z', 'period': 60.0, 'longitude': '4.41788', 'nrOfLanes': '2', 'measurementSiteName': 'R_RWS_0610', 'latitude': '52.1431', 'averageFlow': 12.0, 'averageSpeed': 32.0};
+let datapoint = {'lane': 'lane2', 'measurementSiteId': 'GEO02_R_RWSTI610', 'measurementTime': '2017-01-20T06:11:00Z', 'period': 60.0, 'longitude': 4.41788, 'nrOfLanes': '2', 'measurementSiteName': 'R_RWS_0610', 'latitude': 52.1431, 'averageFlow': 12.0, 'averageSpeed': 32.0};
 
+/**
+ * WARNING: the map component used longitude, latitude format for coordinates :/
+ */
 class Map extends Component {
 
     render() {
@@ -24,6 +27,7 @@ class Map extends Component {
                 style={style}
                 center={[5.175500, 52.078689]}
                 zoom={[8]}
+                pitch={30}
                 //Not needed
                 accessToken="pk.eyJ1IjoiZmFicmljOCIsImEiOiJjaWc5aTV1ZzUwMDJwdzJrb2w0dXRmc2d0In0.p6GGlfyV-WksaDV_KdN27A"
                 containerStyle={{
@@ -31,14 +35,20 @@ class Map extends Component {
                     width: "100vw"
                 }}>
                 <Layer
-                    type="symbol"
-                    id="marker"
-                    layout={{ "icon-image": "marker-15" }}>
-                    <Feature coordinates={[Number(datapoint.longitude), Number(datapoint.latitude)]}/>
-                    <Feature coordinates={[5.175500, 52.078689]}/>
-                    <ZoomControl/>
-                    <ScaleControl/>
+                    type="line"
+                    layout={{ "line-cap": "round", "line-join": "round" }}
+                    paint={{ "line-color": "#4790E5", "line-width": 12 }}>
+                    <Feature coordinates={[[5.175500, 52.078689], [5.2, 52.1]]}/>
                 </Layer>
+                <Layer
+                    type="circle"
+                    paint={{ "circle-radius": 30, "circle-color": "#E54E52", "circle-opacity": .8 }}>
+                    <Feature coordinates={[5.175500, 52.078689]}/>
+                </Layer>
+                <Marker
+                    coordinates={[5.175500, 52.078689]}>
+                    <h1>TEST</h1>
+                </Marker>
             </ReactMapboxGl>
         );
     }
